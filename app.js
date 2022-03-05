935d5b8f94ab093bf77748c3bd7b4ca0f407268f
Gaurav Patil <gaurvpatil17@gmail.com>
2022-03-05 21:59:24 +0530

    make it better2

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const adminRoutes = require("./routes/admin")
 const connectDB = require("./db/connect");
 require("dotenv").config();
 let cors = require("cors");
-
+const path = require("path")
 
 //middleWare
 app.use(express.json());
@@ -37,7 +37,19 @@ app.use("/user", userRoutes , otpRoutes , subscriberRoute , adminRoutes );
 
 // ....................deployment ....................//
 
+ const __dirname1 = path.resolve()
+ 
 
+ if(process.env.NODE_ENV === "production"){
+app.use( express.static(path.join(__dirname1 , "./frontend/build")) )
+app.get("*" , (req,res)=>{
+  res.sendFile(path.resolve(__dirname1 , "./frontend/build/index.html"))
+})
+ }else{
+   app.get("/" , (req,res)=>{
+     res.send("API is runing")
+   })
+ }
 
 
 
